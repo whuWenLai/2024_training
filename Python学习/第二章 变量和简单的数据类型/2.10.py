f404828cfca7aefb556001f1fb7b43b71554ed09
whu_wenlai <2095481018@qq.com>
2024-06-05 20:57:14 +0800

    第二章作业

---
+++ Python学习/第二章 变量和简单的数据类型/2.10.py
@@ -0,0 +1,3 @@
+lucky_number=7
+message=f"我最喜欢的数字是：{lucky_number}"
+print(message)
---
+++ Python学习/第二章 变量和简单的数据类型/2.11_2.12.py
@@ -0,0 +1,8 @@
+# 这是一个注释
+print("一个注释")
+
+'''
+这是多行注释
+hello
+'''
+print("python之禅：The Zen of Python, by Tim Peters\nBeautiful is better than ugly.\nExplicit is better than implicit.\nSimple is better than complex.\n......\n")
